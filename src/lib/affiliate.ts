62415c44c81d08f89f7a3bef4006ca18544b97c0
Bambang <bambangnurdiann@gmail.com>
2026-06-01 22:14:36 +0800

    Fix Blibli

--- src/lib/affiliate.ts
+++ src/lib/affiliate.ts
@@ -18,7 +18,7 @@ export function generateBlibliAffiliateLink(productName: string): string {
   const keyword = encodeURIComponent(productName);
   const blibliSearchUrl = `https://www.blibli.com/jual/${keyword.replace(/%20/g, '-').toLowerCase()}`;
   
-  const deepLinkUrl = `https://blibliaffiliate.onelink.me/JLcX?af_force_deeplink=true&af_param_forwarding=true&af_dp=blibli%3A%2F%2F${encodeURIComponent(blibliSearchUrl.replace('https://www.blibli.com', ''))}&at_fnlp_parameter_name=af_r&af_r=${encodeURIComponent(blibliSearchUrl)}&utm_source=affiliates&utm_medium=affb_6841170ad1b1481a31281e8b&utm_campaign=business_share`;
+  const deepLinkUrl = `https://blibliaffiliate.onelink.me/JLcX?af_force_deeplink=true&af_param_forwarding=true&af_dp=blibli%3A%2F%2F${encodeURIComponent(blibliSearchUrl.replace('https://www.blibli.com', ''))}&at_fnlp_parameter_name=af_r&af_r=${encodeURIComponent(blibliSearchUrl)}&utm_source=affiliates&utm_medium=affb_6841170ad1b1481a31281e8b&utm_campaign=business_share&utm_content={psn}-{clickid}`;
 
   return `https://atid.me/00dh9j002poy?url=${encodeURIComponent(deepLinkUrl)}`;
 }
